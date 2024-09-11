07c854783d90c362a7373c3a14b43d6466fcd2b1
Renan Ameko <130808290+devRenanAmeko@users.noreply.github.com>
2024-09-11 13:35:00 -0300

    Add files via upload

---
+++ public/home.php
@@ -0,0 +1,19 @@
+<?php
+defined('CONTROL') or die('Acesso negado');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplicação</title>
+</head>
+<body>
+
+    <?php require 'nav.php' ?>
+
+    <h3>Bem vindo a aplicação</h3>
+    [conteudo]
+</body>
+</html>
